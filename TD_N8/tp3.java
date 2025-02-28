478c73c9527ac8cb70164ab3097aa55cfe61360a
Your Name <mahmoudsaadaui55@gmail.com>
2025-02-28 11:15:48 +0100

    organisation des dossirs

--- TD_N8/tp3.java
+++ TD_N8/tp3.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
-public class tp2 {
+public class tp3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Saisir un texte : ");
-        String texte = scanner.nextLine();
+        StringBuffer texte = new StringBuffer(scanner.nextLine());
         System.out.print("Donner mot 1 : ");
         String mot1 = scanner.nextLine();
         System.out.print("Donner mot 2 : ");
         String mot2 = scanner.nextLine();
-        if (!texte.contains(mot1)) {
+        int x = texte.indexOf(mot1);
+        if (x == -1) {
             System.out.println("Mot 1 n'existe pas dans le texte.");
         } else {
-            texte = texte.replace(mot1, mot2);
+            while (x != -1) {
+                texte.replace(x, x + mot1.length(), mot2);
+                x = texte.indexOf(mot1, x + mot2.length());
+            }
             System.out.println("Le nouveau texte est : " + texte);
         }
         scanner.close();
