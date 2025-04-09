ffe2608b779b1c482f4af87abd9c1564adc8f44c
saadaouimahmoid <mahmoudsaadaui55@gmail.com>
2025-04-09 15:16:16 +0100

    Delete TD_9 directory

--- TD_9/ex3_.java
+++
@@ -1,7 +0,0 @@
-public class ex3_ {
-    public static void main(String[] args) {
-        int n = Methode.lectureN();
-        int somme = Methode.calculeSomme(n);
-        System.out.println("La somme des " + n + " premiers carrés est : " + somme);
-    }
-}
